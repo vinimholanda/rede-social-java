de85611173e7d230f4a279c0b4733b05864e7128
Vini <73549297+vmaiaaa@users.noreply.github.com>
2022-06-01 02:36:36 -0300

    Add files via upload

--- bckp.java
+++ bckp.java
@@ -77,26 +77,74 @@ public class bckp {
                         
                                     switch(e){
                                         case 1:
+
+                                        while(true){
+                                            
                                         System.out.print("Digite seu novo nome: ");
                                         String x = teclado.nextLine();
-                                        uTemp.setNome(x);
+
+                                        boolean isNumeric = x.chars().allMatch( Character::isDigit );
+                    
+                                            if (isNumeric || x.equals(uTemp.getNome())) {
+                                                System.out.println("\n(!) Seu nome nao pode ser somente numeros e não pode ser igual ao primeiro.\n");
+                                                continue;
+                                            } else {
+                                                uTemp.setNome(x);
+                                                break;
+                                            }
+                                        }
     
-                                        System.out.print("== Nome alterado com sucesso.");
+                                        System.out.print("\nNome alterado com sucesso.\n");
                                         break;
-    
+
                                         case 2:
-                                        System.out.print("Digite seu novo login: ");
-                                        String y = teclado.nextLine();
-                                        uTemp.setLogin(y);
+                                        while(true){
+
+                                            System.out.print("Digite seu novo login: ");
+                                            String y = teclado.nextLine();
+
+                                            if (y.contains("@") && y.contains(".") && (!y.equals(uTemp.getLogin()))) {
+                                                uTemp.setLogin(y);
+                                                break;
+                                            } else {
+                                                System.out.println("\n(!) Seu login deve ser um email valido e nao pode ser igual ao ultimo.\n");
+                                                continue;
+                                            }
+                                        }
     
-                                        System.out.print("== Login alterado com sucesso.");
+                                        System.out.print("\nLogin alterado com sucesso.\n");
                                         break;
     
                                         case 3:
-                                        System.out.print("Digite sua nova senha: ");
-                                        String z = teclado.nextLine();
-                                        uTemp.setSenha(z);
-                                        System.out.print("== Senha alterada com sucesso");
+
+                                        while(true){
+
+                                            System.out.print("Digite sua nova senha: ");
+                                            String zz = teclado.nextLine();
+    
+                                            boolean isNumeric2 = zz.chars().allMatch( Character::isDigit );
+
+                                            if((zz.contains("@") || zz.contains("#") || zz.contains("$") || zz.contains("%") || zz.contains("&") || zz.contains("*"))){
+                                                
+                                                if ((zz.contains("1") || zz.contains("2") || zz.contains("3") || zz.contains("4") || zz.contains("5") || zz.contains("6") || zz.contains("7") || zz.contains("8") || zz.contains("9"))) {
+                                                    
+                                                    if (!zz.equals(uTemp.getSenha())) {
+                                                        uTemp.setSenha(zz);
+                                                        break;
+                                                    } else {
+                                                        System.out.println("\n(!) Sua senha deve conter no minimo 1 numero, 1 caractere especial (@, #, $, %, &, *) e nao pode ser igual a ultima.\n");
+                                                    }
+
+                                                } else {
+                                                    System.out.println("\n(!) Sua senha deve conter no minimo 1 numero, 1 caractere especial (@, #, $, %, &, *) e nao pode ser igual a ultima.\n");
+                                                }
+                    
+                                            }else{
+                                                System.out.println("\n(!) Sua senha deve conter no minimo 1 numero, 1 caractere especial (@, #, $, %, &, *) e nao pode ser igual a ultima.\n");
+                                            }
+                                        }
+                                        
+                                        System.out.print("\nSenha alterada com sucesso\n");
                                         break;
                                     
                                         case -1:
